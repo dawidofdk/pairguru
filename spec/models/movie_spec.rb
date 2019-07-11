c3385f7cbe237a50a04160e1b78cd6b9894e267d
dawidofdk <dawidof@ukr.net>
2019-07-12 01:01:32 +0200

    Delegate rating, plot and poster from remote movie to movie model

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe Movie do
+  let(:movie) { build(:movie, title: "Godfather") }
+
+  describe "#remote_movie delegate attributes", :vcr do
+    context "when remote movie found" do
+      it "returns proper values" do
+        expect(movie.rating).to eq 9.2
+        expect(movie.plot).to eq "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+        expect(movie.poster).to eq "https://pairguru-api.herokuapp.com/godfather.jpg"
+      end
+    end
+
+    context "when remote movie not found" do
+      let(:movie) { build(:movie, title: "404 in response") }
+
+      it "returns nil values" do
+        expect(movie.rating).to be_nil
+        expect(movie.plot).to be_nil
+        expect(movie.poster).to be_nil
+      end
+    end
+  end
+end
