6fcd80c6056fdbd4fb3c1da943f43c12453b7da3
dawidofdk <dawidof@ukr.net>
2019-07-12 01:01:32 +0200

    Delegate rating, plot and poster from remote movie to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -14,4 +14,23 @@
 
 class Movie < ApplicationRecord
   belongs_to :genre
+  delegate *Remote::Movie::ATTRIBUTES, to: :remote_movie, allow_nil: true
+
+  def poster
+    return unless remote_movie&.poster
+
+    [ENV["REMOTE_MOVIE_IMAGES_URL"], remote_movie&.poster].join
+  end
+
+  private
+
+  def remote_movie
+    @remote_movie ||= Rails.cache.fetch("remote_movie/" + Digest::MD5.hexdigest(title), expires_in: 2.minutes) do
+      Remote::Movie.find(id: title)
+    end
+  rescue Flexirest::HTTPClientException, Flexirest::HTTPServerException => e
+    # TODO: implement error notifier service. (e.g. sentry)
+    Rails.logger.error("API returned #{e.status} : #{e.result.message} - Title: #{title}")
+    nil
+  end
 end
