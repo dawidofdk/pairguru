4f2f6c9b966aa08d01e59bf87cf961a4334102c9
Dmytro Koval <d.koval@medical-tribune.pl>
2019-07-12 15:27:12 +0200

    Add top 10 commenters

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -11,4 +11,29 @@ RSpec.describe "Comments", type: :request do
       expect(page).to have_selector(".commented_user", text: comment.user.email)
     end
   end
+
+  describe "display top 10 commenters" do
+    before do
+      create_list :user, 5
+      create_list :movie, 5
+
+      User.limit(3).pluck(:id).each do |user_id|
+        create :comment, movie: Movie.first, user_id: user_id
+      end
+
+      Movie.offset(1).limit(3).pluck(:id).each do |movie_id|
+        create :comment, movie_id: movie_id, user: User.second
+      end
+
+      create :comment, movie: Movie.last, user: User.third
+    end
+
+    it "displays  top 10 commenters" do
+      visit "/top_10_commenters"
+
+      expect(page.body.gsub(/<\/?[^>]+>/, '')).to include("1.\n#{User.second.email}\n4")
+      expect(page.body.gsub(/<\/?[^>]+>/, '')).to include("2.\n#{User.third.email}\n2")
+      expect(page.body.gsub(/<\/?[^>]+>/, '')).to include("3.\n#{User.first.email}\n1")
+    end
+  end
 end
